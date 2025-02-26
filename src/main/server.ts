e3e70fcc4de111d2ef8c81e069295d0f5c4786d9
Reza 1290 <reza.muktasib@gmail.com>
2025-02-27 00:39:50 +0700

    add: Express adapter

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,7 +1,7 @@
 import 'module-alias/register'
-import dbConnection from '../../infra/db/mongodb/helpers/db-connection'
-import env from '../config/env'
-import setupApp from '../config/app'
+import dbConnection from '../infra/db/mongodb/helpers/db-connection'
+import env from './config/env'
+import setupApp from './config/app'
 
 dbConnection.connect(env.mongodbUrl).then(
     async () => {
