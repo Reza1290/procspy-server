8c44d872fdca3d939daa79856ebc3b1cc758132c
Reza 1290 <reza.muktasib@gmail.com>
2025-06-10 19:22:19 +0700

    fix: userAgent mandatory:

--- src/main/factories/controllers/session-details/create-or-update-session-detail/validation-factory.ts
+++ src/main/factories/controllers/session-details/create-or-update-session-detail/validation-factory.ts
@@ -4,7 +4,8 @@ import { ValidationComposite } from "@infra/http/validations/ValidationComposite
 export const makeCreateOrUpdateSessionDetailValidation = ():ValidationComposite => {
     return new ValidationComposite([
         new RequiredFieldValidation('token'), 
-        new RequiredFieldValidation('ipAddress'), 
+        // new RequiredFieldValidation('ipAddress'), 
+        new RequiredFieldValidation('userAgent'), 
         new RequiredFieldValidation('deviceId') 
       ], 'body')
 }
