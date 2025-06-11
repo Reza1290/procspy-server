423a2ffb12eaacaede7eba01c59dbe9a9d616c2c
Reza 1290 <reza.muktasib@gmail.com>
2025-06-11 12:22:34 +0700

    fix: serve

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,31 +1,36 @@
 import 'module-alias/register';
 import fs from 'fs';
-import httpolyglot, { createServer } from '@httptoolkit/httpolyglot';
 import dbConnection from '../infra/db/mongodb/helpers/db-connection';
 import env from './config/env';
 import setupApp from './config/app';
 import { ok } from 'assert';
 import path from 'path';
 import 'dotenv/config'
-
+import http from 'http';
+import https from 'https';
 
 dbConnection.connect(env.mongodbUrl)
   .then(async () => {
-    await dbConnection.migration()
+    await dbConnection.connect(env.mongodbUrl);
+    await dbConnection.migration();
 
     const app = setupApp();
+    app.set('trust proxy', true); 
 
-    const httpsOptions = {
-      key: fs.readFileSync(__dirname + '../../../certs/key.pem'),
-      cert: fs.readFileSync(__dirname + '../../../certs/cert.pem'),
-    };
+    if (process.env.NODE_ENV === 'development') {
+      const httpsOptions = {
+        key: fs.readFileSync(path.resolve(__dirname, '../../../certs/key.pem')),
+        cert: fs.readFileSync(path.resolve(__dirname, '../../../certs/cert.pem')),
+      };
 
-    createServer(httpsOptions, app) // Use createServer directly
-      .listen(env.port, () => {
-        console.log(`✅ HTTP/HTTPS server running on port ${env.port}`);
-        console.log(`   Try:`);
-        console.log(`   - http://localhost:${env.port}`);
-        console.log(`   - https://localhost:${env.port}`);
+      https.createServer(httpsOptions, app).listen(env.port, () => {
+        console.log(`Development HTTPS server running on https://localhost:${env.port}`);
       });
+    } else {
+      http.createServer(app).listen(env.port, () => {
+        console.log(` Production HTTP server running on port ${env.port}`);
+        console.log(` proxied rawr `);
+      });
+    }
   })
   .catch(console.error);
