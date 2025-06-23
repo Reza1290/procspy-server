3de072691d87fb56c56fa62663dcb25ee462c064
Reza 1290 <reza.muktasib@gmail.com>
2025-06-23 22:45:39 +0700

    fix: session status

--- src/application/use-cases/logs/CreateLogByToken.ts
+++ src/application/use-cases/logs/CreateLogByToken.ts
@@ -9,29 +9,33 @@ export class CreateLogByToken implements CreateLogByTokenInterface {
     constructor(
         private readonly createLogRepository: CreateLogRepository,
         private readonly getSessionByTokenRepository: GetSessionByTokenRepository,
-        
+
     ) { }
 
     async execute(body: CreateLogByTokenInterface.Request): Promise<CreateLogByTokenInterface.Response> {
         const { flagKey, token, attachment, logType } = body
-        
+
         const isSessionExist = await this.getSessionByTokenRepository.getSessionByToken(token)
 
-        if(!isSessionExist){
+        if (!isSessionExist) {
             return new SessionNotExistError()
         }
 
-        if(isSessionExist.status !== SessionStatus.Ongoing && isSessionExist.status !== SessionStatus.Paused){
+        if (isSessionExist.status !== SessionStatus.Scheduled && isSessionExist.status !== SessionStatus.Ongoing && isSessionExist.status !== SessionStatus.Paused) {
             return new SessionNotExistError()
         }
 
+        if (isSessionExist.status === SessionStatus.Scheduled && flagKey !== "CONNECT") {
+            return new SessionNotExistError()
+        }
+        
         const newLog = await this.createLogRepository.createLog({
             attachment: attachment ?? "",
             sessionId: isSessionExist.id,
             flagKey: flagKey ?? "DEFAULT",
             logType: logType ?? "System"
         })
-        
+
         return newLog
     }
 }
