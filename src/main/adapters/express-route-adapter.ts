f76aa9ed252f0f73a06c78fd2865ebbc004b84d3
Reza 1290 <reza.muktasib@gmail.com>
2026-03-05 22:52:03 +0700

    fix(cookies): cookies local

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -16,15 +16,17 @@ export const expressRouteAdapter = (
     // return res.status(200).json({
     //     error: httpRequest
     // })
+    const isProd = process.env.NODE_ENV === "production";
+    
     console.log(httpRequest)
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode <= 299) {
         if(httpResponse.statusCode == 200 && httpResponse.body?.authenticationToken){
-            res.cookie('access_token', httpResponse.body?.authenticationToken,{
+            res.cookie("access_token", httpResponse.body?.authenticationToken, {
                 httpOnly: true,
                 secure: true,
-                sameSite: "none",
-                domain: '.procspy.link'
+                sameSite: isProd ? "none" : "lax",
+                domain: isProd ? ".procspy.link" : undefined
             })
         }
         res.status(httpResponse.statusCode).json(httpResponse.body)
